9492793c8f0c56a9b5045f4a483193fce8394f27
Vincent <vincent@vplusplus.nl>
2020-09-28 14:43:23 +0200

    Added debug lines

--- Assets/Code/Game.cs
+++ Assets/Code/Game.cs
@@ -15,6 +15,8 @@ public class Game : MonoBehaviour
 
     private List<Projectile> projectiles;
 
+    private Texture2D pixel;
+
     private void Awake()
     {
         Instance = this;
@@ -22,6 +24,10 @@ public class Game : MonoBehaviour
 
     private void Start()
     {
+        pixel = new Texture2D(1, 1);
+        pixel.SetPixel(0, 0, Color.white);
+        pixel.Apply();
+
         player = new Player();
 
         enemies = new List<Enemy>();
@@ -46,6 +52,37 @@ public class Game : MonoBehaviour
             //Use Sin to animate the colour of the text (GUI.color) between alpha 0.5 and 1.0
             GUI.Label(new Rect(Screen.width * .5f - 50.0f, Screen.height * .5f - 10.0f, 100.0f, 100.0f), "YOU LOSE!");
         }
+        else
+		{
+            if (Input.GetKey(KeyCode.Q))
+            {
+                
+
+                foreach (var enemy in enemies)
+                {
+                    var direction = enemy.Position - player.Position;
+                    var distance = direction.Magnitude;
+
+                    var a = DevMath.DevMath.RadToDeg(DevMath.Vector2.Angle(new DevMath.Vector2(0, 0), direction));
+                    GUIUtility.RotateAroundPivot(a, player.Position.ToUnity());
+
+                    GUI.color = Color.magenta;
+
+                    GUI.DrawTexture(new Rect(player.Position.x, player.Position.y, distance, 1), pixel);
+
+                    GUI.matrix = Matrix4x4.identity;
+
+                    GUI.color = Color.black;
+
+                    var dot = DevMath.Vector2.Dot(direction.Normalized, player.Direction);
+
+                    var textPos = player.Position + DevMath.Vector2.DirectionFromAngle(a) * (distance * .5f);
+                    GUI.Label(new Rect(textPos.x, textPos.y, 200.0f, 200.0f), $"Distance: {distance}\nAngle: {a}\nDot: {dot}");
+                }
+
+                GUI.color = Color.white;
+            }
+		}
     }
 
     public void CreateProjectile(DevMath.Vector2 position, DevMath.Vector2 direction, float startVelocity, float acceleration)
